87adacc943be7b7863f0bf3436ff35b5dc9ef891
Dev-Flo <flo-wauthier@hotmail.fr>
2023-10-29 21:55:18 +0100

    add project

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -6,10 +6,17 @@ import Contact2 from '../Contact/Contact2';
 
 export default function Project() {
 
+        const iframeSource = "https://immopros-app.com";
+
+
+
     return (
         <div className="accueil flex flex-col gap-1 justify-center items-center">
             <div className="accueil-navbar">
                 <Navbar2 />  
+            </div>
+            <div> 
+            <iframe src={iframeSource} height="300" width="300" title="Immo'Pros"/>   
             </div>
             <h1 className="title ">Projets</h1>
             <Contact2 />
