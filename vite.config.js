f2b9c775fc7245ce07408abc0720ffe478610af9
Dev-Flo <flo-wauthier@hotmail.fr>
2023-10-29 19:36:26 +0100

    setup files for deploy

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // ...
+  build: {
+    outDir: 'dist', // Par défaut, le répertoire de build est "dist"
+  },
+});
