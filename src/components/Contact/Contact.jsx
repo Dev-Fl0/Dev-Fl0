b95f2b8d71cba663d1f290729624990db66c53de
Dev-Flo <flo-wauthier@hotmail.fr>
2023-10-29 21:16:01 +0100

    fix path icones

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import emailIcone from '../../../src/assets/icons/email_logo1.png';
 import linkedinIcone from '../../../src/assets/icons/linkedin_logo.png';
 import twitterIcone from '../../../src/assets/icons/twitter_logo.png';
-import phoneIcone from '../../../src/assets/icons/smartphone_logo_logo.png';
+import phoneIcone from '../../../src/assets/icons/smartphone_logo.png';
 
 export default function Contact() {
 
