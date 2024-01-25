85d145bb913645d46183e7975c3a01c730e5cf51
Dev-Fl0 <flo-wauthier@hotmail.fr>
2024-01-25 13:06:33 +0100

    add new project

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -2,24 +2,20 @@ import React from 'react';
 import Navbar2 from '../Navbar/Navbar2';
 import Contact2 from '../Contact/Contact2';
 import ImmoPros from './ImmoPros';
-
-
-
+import GitHubSearch from './GitHubSearch';
 
 export default function Project() {
-
-
-    return (
-        <div className="accueil flex flex-col gap-1 justify-center items-center">
-            <div className="accueil-navbar">
-                <Navbar2 />  
-            </div>
-            <h1 className="title title-page-project">Projets</h1>
-            <div className='project-div flex gap-6 justify-between'>    
-               <ImmoPros />
-            </div>
-            <Contact2 />
-        </div>
-        
-    );
-};
+  return (
+    <div className="accueil flex flex-col gap-1 justify-center items-center">
+      <div className="accueil-navbar">
+        <Navbar2 />
+      </div>
+      <h1 className="title title-page-project">Projets</h1>
+      <div className="project-div flex gap-6 justify-between">
+        <ImmoPros />
+        <GitHubSearch />
+      </div>
+      <Contact2 />
+    </div>
+  );
+}
