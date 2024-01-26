98c188b16855ecaf9e83ef490fe3750836809c87
Dev-Fl0 <flo-wauthier@hotmail.fr>
2024-01-26 09:00:47 +0100

    remove tudum

--- src/components/Button/EnterButon.jsx
+++ src/components/Button/EnterButon.jsx
@@ -1,42 +1,34 @@
 import React from 'react';
 
 // Audio
-import Tudum from '../../audio/tudum.mp3'
-
-
-
+// import Tudum from '../../audio/tudum.mp3';
 
 export default function EnterButton() {
+  const handleClick = () => {
+    const logoElement = document.querySelector('.logo-container');
+    const btnElement = document.querySelector('.welcome-container');
 
-    const handleClick = () => {
-        const tudum = new Audio(Tudum);
-        tudum.play();
-        const logoElement = document.querySelector('.logo-container');
-        const btnElement = document.querySelector('.welcome-container');
-        
-        setTimeout(() => {
-            document.body.classList.add('done');
+    setTimeout(() => {
+      document.body.classList.add('done');
+    }, 1600);
 
-        }, 1600)
-        
-        setTimeout(() => {
-            logoElement.classList.add('logo-done');
-            btnElement.classList.add('welcome-done');
-        }, 3500)
+    setTimeout(() => {
+      logoElement.classList.add('logo-done');
+      btnElement.classList.add('welcome-done');
+    }, 3500);
 
+    document.body.classList.add('run');
+  };
 
-        document.body.classList.add('run');
-
-    };
-
-    return (
-        <div className='welcome-container'>
-            <button 
-            type="button" 
-            className="btn bg-red-600 hover:bg-red-700 sm:items-start text-white font-semibold py-2 px-4 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105"
-            onClick={handleClick}>
-            Entrer
-            </button>
-        </div>
-    );
-};
+  return (
+    <div className="welcome-container">
+      <button
+        type="button"
+        className="btn bg-red-600 hover:bg-red-700 sm:items-start text-white font-semibold py-2 px-4 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105"
+        onClick={handleClick}
+      >
+        Entrer
+      </button>
+    </div>
+  );
+}
--- src/components/Button/ScrollButton.jsx
+++ src/components/Button/ScrollButton.jsx
@@ -1,32 +1,24 @@
 import React from 'react';
 
-
-// Audio
-import Tudum from '../../audio/tudum.mp3'
 // import Presentation from '../Presentation/Presentation';
 
-
 export default function ScrollButton() {
-    // const [showPresentation, setShowPresentation] = useState(false);
+  // const [showPresentation, setShowPresentation] = useState(false);
 
+  const handleClick = () => {
+    // setShowPresentation(!showPresentation);
+  };
 
-    const handleClick = () => {
-        const tudum = new Audio(Tudum);
-        tudum.play();
-        // setShowPresentation(!showPresentation);
-
-    };
-    
-    return (
-        <>
-            <button 
-            type="button" 
-            className="btn faceDown bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105"
-            onClick={handleClick}>
-            Scroll
-            </button>
-            {/* {showPresentation && <div><Presentation /></div>}   */}
-        </>
-    );
-
-};
+  return (
+    <>
+      <button
+        type="button"
+        className="btn faceDown bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:scale-105"
+        onClick={handleClick}
+      >
+        Scroll
+      </button>
+      {/* {showPresentation && <div><Presentation /></div>}   */}
+    </>
+  );
+}
