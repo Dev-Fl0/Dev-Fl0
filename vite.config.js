1e24dbcce26025f330734fbd1a57bcef4d8676c3
Dev-Fl0 <flo-wauthier@hotmail.fr>
2024-01-29 19:36:12 +0100

    V2

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  // ...
-  build: {
-    outDir: 'dist', // Par défaut, le répertoire de build est "dist"
-  },
-});
